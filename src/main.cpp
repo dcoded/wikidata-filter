7bc1cec828a6e86a722dabe30228f9fe8afcde47
dcoded <dcoded@users.noreply.github.com>
2015-09-30 21:58:10 -0700

    line scanning feedback

--- src/main.cpp
+++ src/main.cpp
@@ -22,22 +22,25 @@ int main(int argc, char** argv)
     const char* filename = argv[1];
     const char* category = argv[2];
 
+    std::ifstream file;
+    file.exceptions(std::ios::failbit | std::ios::badbit);
+    file.open(filename, std::ios_base::in | std::ios_base::binary);
+
     filtering_stream<input> decompressor;
-    gunzip(filename, decompressor);
+    decompressor.push(boost::iostreams::gzip_decompressor());
+    decompressor.push(file);
 
     std::vector<std::string> categories = { category };
 
-    int i = 0;
+    int i = 1;
     for(std::string line; getline(decompressor, line); i++)
     {
-        std::cout << "Scanning line..." << i << "          \r";
+        std::cout << "Scanning line..." << i << "\r";
         if(target(line, categories))
         {
-            std::cout << line << "\n";
+            std::cout << "\n";
+            std::cout << "\r" << line << "\n";
         }
-
-
-        std::cout << '[' << i << ']' << ' ' << line << std::endl;
     }
 }
 
